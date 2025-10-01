6508607384ba0cf8ad5d0edfbaafded27e91f3ca
Angelo Pirola <angelo9871@gmail.com>
2025-10-02 00:03:34 +0200

    Refactor ToLocationViewModel to use expression-bodied syntax
    Refactored the `ToLocationViewModel` method in the `LocationExtension` class to use the more concise expression-bodied member syntax. Replaced the traditional method body with curly braces and a `return` statement with the `=>` operator for improved readability and simplicity.

--- WebApi.Comuni/Models/Extensions/LocationExtension.cs
+++ WebApi.Comuni/Models/Extensions/LocationExtension.cs
@@ -3,7 +3,5 @@ namespace WebApi.Comuni.Models.Extensions;
 public static class LocationExtension
 {
 	public static LocationViewModel ToLocationViewModel(this Location location)
-	{
-		return new LocationViewModel(location.ComuneId, location.Comune, location.Cap, location.Provincia, location.Regione);
-	}
+		=> new(location.ComuneId, location.Comune, location.Cap, location.Provincia, location.Regione);
 }
